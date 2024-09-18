73a766ffc7673f1a0eb893e0dc0be2f48199b4e7
sanmijingbrahma <bsanmijing@gmail.com>
2024-09-18 18:57:54 +0530

    Add feature to be able to play lcocal music using local module

--- mood_player.py
+++ mood_player.py
@@ -1,4 +1,33 @@
 import argparse
+import os
+import random
+import pygame
+
+def play_music(mood):
+    music_dir = 'music'
+    mood_dir = os.path.join(music_dir,mood)
+
+    if(not os.path.exists(mood_dir)):
+        print(f"No Music avalilable for mood : {mood}")
+        return
+    
+    files = [f for f in os.listdir(mood_dir) if f.endswith(".mp3")]
+    if not files:
+        print(f"No music file for mood : {mood}")
+        return
+    
+    random_file = random.choice(files)
+    file_path = os.path.join(mood_dir,random_file)
+    print(f'Playing {random_file} for mood : {mood}')
+    
+    pygame.mixer.init() #initializing the mixer module which is responsible for handling audio files
+    pygame.mixer.music.load(file_path) # loaded the file path
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    
 
 def main():
 
@@ -12,6 +41,7 @@ def main():
     arg = parser.parse_args()
 
     print(f"Your current mood is : {arg.mood}")
+    play_music(arg.mood)
 
 
 
